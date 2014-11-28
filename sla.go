71a3b35536a32aa29774b963684c046879ad3e92
validname <validname@users.noreply.github.com>
2014-11-28 17:02:03 +0600

    reformat sla.go + rename function

--- sla.go
+++ sla.go
@@ -4,18 +4,17 @@ package zabbix
 
 // https://www.zabbix.com/documentation/2.0/manual/appendix/api/history/definitions
 type SLA struct {
-	From      float64 `json:"from,omitempty"`
-	To       float64    `json:"to"`
-	Sla       float64 `json:"sla"`
-	OkTime float64    `json:"okTime,omitempty"`
-	ProblemTime float64    `json:"problemTime,omitempty"`
-	DowntimeTime float64    `json:"downtimeTime,omitempty"`
-	HostId	int64 `json:"hostid"`
+	From         float64 `json:"from,omitempty"`
+	To           float64 `json:"to"`
+	Sla          float64 `json:"sla"`
+	OkTime       float64 `json:"okTime,omitempty"`
+	ProblemTime  float64 `json:"problemTime,omitempty"`
+	DowntimeTime float64 `json:"downtimeTime,omitempty"`
+	HostId       int64   `json:"hostid"`
 }
 
-
 // Wrapper for item.get https://www.zabbix.com/documentation/2.0/manual/appendix/api/item/get
-func (api *API) SlaGet(params Params) (res *SLA, err error) {
+func (api *API) GetSla(params Params) (res *SLA, err error) {
 	if _, present := params["output"]; !present {
 		params["output"] = "extend"
 	}
@@ -24,24 +23,24 @@ func (api *API) SlaGet(params Params) (res *SLA, err error) {
 		return
 	}
 
-	ids:=params["serviceids"].(string)
-	k:=response.Result.(map[string]interface{})
-	k2:=k[ids].(map[string]interface{})["sla"]
-	k3:=k2.([]interface{})
-	if len(k3)==0 {
-		return nil,nil
+	ids := params["serviceids"].(string)
+	k := response.Result.(map[string]interface{})
+	k2 := k[ids].(map[string]interface{})["sla"]
+	k3 := k2.([]interface{})
+	if len(k3) == 0 {
+		return nil, nil
 	}
-	k4:=k3[0].(map[string]interface{})
-	k5:=k4
-	
-	res=&SLA{}
+	k4 := k3[0].(map[string]interface{})
+	k5 := k4
 
-	res.From=k5["from"].(float64)
-	res.To=k5["to"].(float64)
-	res.Sla=k5["sla"].(float64)
-	res.OkTime=k5["okTime"].(float64)
-	res.ProblemTime=k5["problemTime"].(float64)
-	res.DowntimeTime=k5["downtimeTime"].(float64)
+	res = &SLA{}
+
+	res.From = k5["from"].(float64)
+	res.To = k5["to"].(float64)
+	res.Sla = k5["sla"].(float64)
+	res.OkTime = k5["okTime"].(float64)
+	res.ProblemTime = k5["problemTime"].(float64)
+	res.DowntimeTime = k5["downtimeTime"].(float64)
 
 	//reflector.MapsToStructs2(response.Result.([]interface{}), &res, reflector.Strconv, "json")
 	return
